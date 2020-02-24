45be4d90e6d005b0ff27ce997dfe19aef6e67a09
ibrahem <ibrahem.reiad@gmail.com>
2020-02-25 01:55:43 +0200

    add search and add style if there is no todos in DB

--- src/app/todos-list/todos-list.component.ts
+++ src/app/todos-list/todos-list.component.ts
@@ -13,17 +13,37 @@ export class TodosListComponent implements OnInit {
   constructor(private todoService: TodosService, private router: Router) { }
 
   todosList: ITodos[] = [];
-
+  filterTodos: ITodos[] = [];
+  private _filterList: string;
   ngOnInit() {
     this.todoService.getAllTodos().subscribe(data => {
       console.log(data);
       this.todosList = data;
+      this.filterTodos = this.todosList;
     });
+  }
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filterTodos = this._filterList ? this.performFilter(this._filterList) : this.todosList;
+  }
+  performFilter(_filterList: string): ITodos[] {
+    return this.todosList.filter((todoList: ITodos) => {
+      // const name = todoList.title.toLowerCase();
+      // const searchTerm = _filterList.toLowerCase();
+      // const firstIndex = name.indexOf(searchTerm);
+      // return firstIndex > -1;
+      return todoList.title.toLowerCase().indexOf(_filterList.toLowerCase()) > -1;
+    })
   }
   goToCreate() {
     this.router.navigate(["/todo"]);
   }
   onDelete(todo) {
-    this.todosList = this.todosList.filter(ele => ele._id !== todo._id)
+    this.filterTodos = this.todosList.filter(ele => ele._id !== todo._id)
   }
 }
